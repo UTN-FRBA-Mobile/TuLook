dc044b6a1e1beeebb745940537f68c7806eb95ea
RamiroAg <ramiro.a.gutierrez@gmail.com>
2021-05-20 01:16:39 -0300

    Se agregó el Navigation Drawer que actualmente permite acceder directamente a cada uno de los fragments

--- app/src/main/java/com/example/tulook/MainActivity.kt
+++ app/src/main/java/com/example/tulook/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.example.tulook
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.tulook.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
+    var drawerLayout: DrawerLayout? = null
+    var actionBarDrawerToggle: ActionBarDrawerToggle? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_TuLook)
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -24,10 +33,27 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        drawerLayout = binding.drawerLayout
+
+        setupDrawerLayout()
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        //GUti: comento para impementar el tuto de ND
+        //return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+    private fun setupDrawerLayout() {
+        binding.navView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
